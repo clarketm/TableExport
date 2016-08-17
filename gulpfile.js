0a39f05534563bbaee6de911190e0d0d2a4aa03d
Travis Clarke <travis.m.clarke@gmail.com>
2016-08-17 01:06:33 -0700

    update gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var gulp = require('gulp'),
+    replace = require('gulp-replace'),
+    bump = require("gulp-bump"),
     rename = require("gulp-rename"),
     del = require('del'),
     css = require('gulp-clean-css');
@@ -22,6 +24,34 @@ gulp.task('js', ['clean'], function () {
             suffix: '.min'
         }))
         .pipe(gulp.dest('./dist/js/'));
+});
+
+gulp.task('bump', ['bump-js', 'bump-css'], function(){
+    return gulp.src(['./bower.json', './package.json', './src/stable.tableexport.js'])
+        .pipe(bump())
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('bump-js', function () {
+    return gulp.src(['src/stable/js/tableexport.js'])
+    .pipe(replace(/(v\d\.\d\.)(\d)/g, function (matches, match1, match2) {
+        return match1 + (Number(match2)+1);
+    }))
+    .pipe(gulp.dest('src/stable/js/'))
+    .on('end', function () {
+        gulp.start('js');
+    });
+});
+
+gulp.task('bump-css', function () {
+    gulp.src(['src/stable/css/tableexport.css'])
+        .pipe(replace(/(v\d\.\d\.)(\d)/g, function (matches, match1, match2) {
+            return match1 + (Number(match2) + 1);
+        }))
+        .pipe(gulp.dest('src/stable/css/'))
+        .on('end', function () {
+            gulp.start('css');
+        });
 });
 
 gulp.task('clean', function () {
