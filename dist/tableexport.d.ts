85d2af1eb1a7fb5880b2b4b3ecede64f0504d659
Travis Clarke <travis.m.clarke@gmail.com>
2018-10-24 00:58:34 -0700

    v5.0.3 - fix issue when $.fn.tableExport (i.e. jquery prototype) being ignored, closes #143

--- dist/tableexport.d.ts
+++ dist/tableexport.d.ts
@@ -1,9 +1,9 @@
-// Type definitions for TableExport v5.0.2
+// Type definitions for TableExport v5.0.3
 // Project: https://tableexport.travismclarke.com
 // Definitions by: Travis Clarke <https://github.com/clarketm>
 
 /*!
- * TableExport.js v5.0.2 (https://www.travismclarke.com)
+ * TableExport.js v5.0.3 (https://www.travismclarke.com)
  *
  * Copyright (c) 2018 - Travis Clarke - https://www.travismclarke.com
  *
