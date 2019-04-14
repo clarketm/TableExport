528d589cb276f057a25716b14856fc4cf6948009
fedeTibaldo <tibaldo125@gmail.com>
2019-04-14 17:15:11 +0200

    Rename module

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,37 @@
+/**
+ * @license Apache-2.0
+ * @copyright Travis Clarke <travis.m.clarke@gmail.com> (https://www.travismclarke.com/)
+ */
+
+/**
+ * @author fedeTibaldo <fedetibaldo@protonmail.com>
+ * @summary Summary of changes
+ * @description
+ * Summary of changes:
+ * - Rename entrypoint files to match the new module name.
+ * - Flatten `src` directory by making the `/stable` branch the only one available.
+ * - Remove `css` tasks and imports
+ */
+
 var gulp = require("gulp"),
   replace = require("gulp-replace"),
   bump = require("gulp-bump"),
   rename = require("gulp-rename"),
   del = require("del"),
-  css = require("gulp-clean-css"),
   js = require("gulp-uglify");
 
-gulp.task("css", ["clean"], function() {
-  return gulp.src("./src/stable/css/tableexport.css")
-    .pipe(gulp.dest("./dist/css/"))
-    .pipe(css())
-    .pipe(rename({
-      suffix: ".min"
-    }))
-    .pipe(gulp.dest("./dist/css/"))
-    .pipe(gulp.dest("./src/stable/css/"));
-});
-
 gulp.task("js", ["clean"], function() {
-  return gulp.src("./src/stable/js/tableexport.js")
-    .pipe(gulp.dest("./dist/js/"))
+  return gulp.src("./src/browser-xlsx.js")
+    .pipe(gulp.dest("./dist/"))
     .pipe(js({ output: { comments: /^!|@preserve|@license|@cc_on/i } }))
     .pipe(rename({
       suffix: ".min"
     }))
-    .pipe(gulp.dest("./dist/js/"))
-    .pipe(gulp.dest("./src/stable/js/"));
+    .pipe(gulp.dest("./dist/"))
+    .pipe(gulp.dest("./src/"));
 });
 
-gulp.task("bump-all", ["bump", "bump-js", "bump-css", "bump-typings", "bump-readme"]);
+gulp.task("bump-all", ["bump", "bump-js", "bump-typings", "bump-readme"]);
 
 gulp.task("bump", function() {
   return gulp.src(["./bower.json", "./package.json"])
@@ -37,32 +40,21 @@ gulp.task("bump", function() {
 });
 
 gulp.task("bump-js", function() {
-  return gulp.src(["src/stable/js/tableexport.js"])
+  return gulp.src(["src/browser-xlsx.js"])
     .pipe(replace(/(v\d+\.\d+\.)(\d+)/g, function(matches, match1, match2) {
       return match1 + (Number(match2) + 1);
     }))
     .pipe(replace(/(version: ["']\d+\.\d+\.)(\d+)/g, function(matches, match1, match2) {
       return match1 + (Number(match2) + 1);
     }))
-    .pipe(gulp.dest("src/stable/js/"))
+    .pipe(gulp.dest("src/"))
     .on("end", function() {
       gulp.start("js");
     });
 });
 
-gulp.task("bump-css", function() {
-  gulp.src(["src/stable/css/tableexport.css"])
-    .pipe(replace(/(v\d+\.\d+\.)(\d+)/g, function(matches, match1, match2) {
-      return match1 + (Number(match2) + 1);
-    }))
-    .pipe(gulp.dest("src/stable/css/"))
-    .on("end", function() {
-      gulp.start("css");
-    });
-});
-
 gulp.task("bump-typings", function() {
-  gulp.src(["dist/tableexport.d.ts"])
+  gulp.src(["dist/browser-xlsx.d.ts"])
     .pipe(replace(/(v\d+\.\d+\.)(\d+)/g, function(matches, match1, match2) {
       return match1 + (Number(match2) + 1);
     }))
@@ -78,12 +70,12 @@ gulp.task("bump-readme", function() {
 });
 
 gulp.task("clean", function() {
-  return del(["dist/css", "dist/js"]);
+  return del(["dist/*.js"]);
 });
 
 gulp.task("test", ["build"]);
 
-gulp.task("build", ["css", "js"]);
+gulp.task("build", ["js"]);
 
 gulp.task("default", ["build"]);
 
--- src/v1/v1.2/js/plugins/ToggleAttr.js
+++
@@ -1,8 +0,0 @@
-(function ($) {
-    $.fn.toggleAttr = function (attr) {
-        return this.each(function () {
-            var $this = $(this);
-            $this.attr(attr) ? $this.removeAttr(attr) : $this.attr(attr, attr);
-        });
-    };
-}(jQuery));
